e88aff7d6896bbb59ef71eea48ef4c2f4aeee7ec
Joshua Lester <jlester3@ewu.edu>
2024-04-13 11:38:15 -0700

    Starting retreive messages endpoint

---
+++ Api/Commands/RetreiveMessagesCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace Api.Commands
+{
+	public class RetreiveMessagesCommand : IRequest<RetreiveMessagesDto>
+	{
+		public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public int OldestMessageId { get; set; }
+
+		public RetreiveMessagesCommand(int senderId, int receiverId, int oldestMessageId)
+		{
+			SenderId = senderId;
+			ReceiverId = receiverId;
+			OldestMessageId = oldestMessageId;
+		}
+	}
+
+	public class RetreiveMessagesDto
+	{
+		public RetreiveMessagesDto() { }
+		public List<Message> Messages { get; set; }
+	}
+}
---
+++ Api/Commands/RetreiveMessagesCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Repos;
+using MediatR;
+
+namespace Api.Commands
+{
+	public class RetreiveMessagesCommandHandler : IRequestHandler<RetreiveMessagesCommand, RetreiveMessagesDto>
+	{
+		private readonly MessagesRepository _repository;
+
+		public RetreiveMessagesCommandHandler(MessagesRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public Task<RetreiveMessagesDto> Handle(RetreiveMessagesCommand request, CancellationToken cancellationToken)
+		{
+			ArgumentNullException.ThrowIfNull(request);
+
+			return HandleInternalAsync(request, cancellationToken);
+		}
+
+		private Task<RetreiveMessagesDto> HandleInternalAsync(RetreiveMessagesCommand request, CancellationToken cancellationToken)
+		{
+
+		}
+	}
+}

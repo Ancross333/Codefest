33112959dfa79a2c8a63ba755564b5796c3fe458
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-04-13 11:08:33 -0700

    Add more entities

---
+++ Domain/Values/Interest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Values
+{
+    public  enum Interest
+    {
+        Healthcare,
+        Education,
+        Environment,
+        AnimalWelfare,
+        ArtsAndCulture,
+        DisasterRelief,
+        HumanRights,
+        Technology,
+        SportsAndRecreation,
+        CommunityDevelopment
+    }
+}
---
+++ Domain/Values/Values.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Values
+{
+    public class Values
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public Interest Interest { get; set; }
+    }
+}

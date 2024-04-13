d3dfe028bb8efb5162550f8b3b00717a59c85ad8
Joshua Lester <jlester3@ewu.edu>
2024-04-13 11:51:26 -0700

    Almost finishing retreive messages handler

--- Infrastructure/Repos/MessagesRepository.cs
+++ Infrastructure/Repos/MessagesRepository.cs
@@ -1,4 +1,5 @@
-
+using Domain.Message;
+using Db;
 using Domain.Interfaces;
 
 namespace Infrastructure.Repos
@@ -6,6 +7,7 @@ namespace Infrastructure.Repos
 	public class MessagesRepository : IMessagesRepository
 	{
 		private readonly DinoNuggiesDbContext _dbContext;
+		private const int MaxMessagesRetreivedInQuery = 10;
 
 		public MessagesRepository(DinoNuggiesDbContext dbContext)
 		{
@@ -14,14 +16,15 @@ namespace Infrastructure.Repos
 
 		public List<Message> Get(int senderId, int receiverId, int oldestMessageId)
 		{
-			return _dbContext.Messages.AsParallel().Where(m => IsValidMessage(m, senderId, receiverId, oldestMessageId))
-				.OrderByDescending(m => m.Id).ToList();
+			return _dbContext.Messages.AsParallel().OrderByDescending(m => m.Id)
+				.Where(m => IsValidMessage(m, senderId, receiverId, oldestMessageId))
+				.Take(MaxMessagesRetreivedInQuery).OrderBy(m => m.Id).ToList();
 		}
 
 		private static bool IsValidMessage(Message message, int senderId, int receiverId, int oldestMessageId)
 		{
-			return (message.senderId == senderId || message.senderId == receiverId) 
-				&& (message.receiverId == receiverId || message.receiverId == senderId)
+			return (message.SenderId == senderId || message.SenderId == receiverId) 
+				&& (message.ReceiverId == receiverId || message.ReceiverId == senderId)
 				&& message.Id < oldestMessageId;
 		}
 	}
